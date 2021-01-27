3b68b3d90c8a674e075817d940aaa392d67e2ec1
HuyDangTu <66896891+HuyDangTu@users.noreply.github.com>
2021-01-27 11:54:13 +0700

    Adjust user action 2

--- client/src/actions/user_action.js
+++ client/src/actions/user_action.js
@@ -26,7 +26,7 @@ export function registerUser(dataToSubmit){
 }
 
 export function loginUser(dataToSubmit){
-    const request = axios.post(`${USER_SERVER}/login`,dataToSubmit)
+    const request = axios.post(`https://myreactsocialnetwork.herokuapp.com/api/users/login`,dataToSubmit)
     .then(response => response.data);
     console.log(request);
     return{
