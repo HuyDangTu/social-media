ca01d83dc02c9bcea054f4442092557f0567b2a0
HuyDangTu <66896891+HuyDangTu@users.noreply.github.com>
2021-01-28 13:40:55 +0700

    mail change

--- server/ultils/mail/reset_template.js
+++ server/ultils/mail/reset_template.js
@@ -1,6 +1,6 @@
 const resetpass = (data) =>{
     
-    const URL = process.env.NODE_ENV === "production" ? process.env.ROOT_URL : "http://localhost:3000";
+    const URL = process.env.NODE_ENV === "production" ? process.env.ROOT_URL : "https://myreactsocialnetwork.herokuapp.com/";
     
     return `
         <!DOCTYPE html>
