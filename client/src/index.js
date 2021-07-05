e9e8ca692dedc8ebecdf86eddc4c696909f5e6fc
Dang Khoa <66207360+KhoaVoDang@users.noreply.github.com>
2021-07-05 17:19:26 +0700

    heroku deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers/index'
 
+
 //HOC 
 //syntax cũ để tạo Store có sử dụng 2 middleware là promisMiddleware và ReduxThunk
 // Trong rect redux 3.1 trở lên sử dụng createStore(reducer,state,applyMiddleware(middleware1,middleware2,..))
