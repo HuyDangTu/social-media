616081058e98122ae8c30ffafb20059cb69909d9
Dang Khoa <66207360+KhoaVoDang@users.noreply.github.com>
2021-07-10 17:36:13 +0700

    2nd commit

--- server/server.js
+++ server/server.js
@@ -3977,12 +3977,12 @@ app.get('/api/statistics/percentageOfAge', auth, admin, async (req, res) => {
 
 
 // const port = process.env.PORT || 3002;
-if(process.env.NODE_ENV === 'production'){
-    const path = require('path');
-    app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client','src','index.js'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     const path = require('path');
+//     app.get('/*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'../client','src','index.js'))
+//     })
+// }
 
 // app.use(function (req, res, next) {
 //     res.status(404).send('Unable to find the requested resource!');
